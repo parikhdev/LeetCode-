2e88faf5b5d902df2a2f4808b7b1a0a57d60c3b1
parikhdev <itsyashparikh@gmail.com>
2025-12-02 22:36:08 +0530

    Added Buying_Stocks.py

---
+++ Buying_Stocks.py
@@ -0,0 +1,15 @@
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0        
+    for price in prices:
+        if price < min_price:
+            min_price = price
+            
+        profit = price - min_price
+        
+        if profit > max_profit:
+            max_profit = profit
+                
+    return max_profit
+
+print(maxProfit([1,2,3,4,5]))
