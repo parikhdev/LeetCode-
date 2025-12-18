d5c59673e48adfe1b98da3ece194ac39b60523c5
parikhdev <itsyashparikh@gmail.com>
2025-12-18 23:49:08 +0530

    Added FindPeakElement.py

---
+++ FindPeakElement.py
@@ -0,0 +1,12 @@
+def findPeakElement(nums):
+    left  = 0
+    right = len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] < nums[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return left
+    
+print(findPeakElement([1,2,3,1,4,56,7,8]))
