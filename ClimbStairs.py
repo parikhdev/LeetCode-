087ebcf69e4ac965fe62a3419360ef121daf8d8a
parikhdev <itsyashparikh@gmail.com>
2026-01-06 22:50:11 +0530

    Added ClimbStairs.py

---
+++ ClimbStairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def climbStairs(self, n):
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        dp = [0] * n
+        dp[0], dp[1] = 1, 2
+
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[-1]
+tests = [1, 2, 3, 4, 5, 10]
+
+for n in tests:
+    print(f"Input: n = {n}")
+    print("Output:", Solution().climbStairs(n))
+    print()
