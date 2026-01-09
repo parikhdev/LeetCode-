a01de37de15d2d4c3fc19352b0463261260842b9
parikhdev <itsyashparikh@gmail.com>
2026-01-09 23:43:27 +0530

    Added PlusOne.py

--- Maximum_SubArray.py
+++ Maximum_SubArray.py
@@ -1,14 +1,20 @@
 def maxSubArray(nums):
     max_sum = nums[0]
     current_sum = nums[0]
+    # start = 0
+    # best_start = 0
+    # best_end = 0
     for i in range(1, len(nums)):
         num = nums[i]
         if current_sum < 0:
             current_sum = num
+            # start = i
         else:
             current_sum += num
         if current_sum > max_sum:
             max_sum = current_sum
-    return max_sum
+            # best_start = start
+            # best_end = i
+    return max_sum #, nums[best_start:best_end+1]
 
-print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4,9]))
---
+++ PlusOne.py
@@ -0,0 +1,11 @@
+def plusOne(digits):
+    n = len(digits)
+    for i in range(n - 1, -1, -1):
+        digits[i] += 1
+        digits[i] %= 10
+        if digits[i] != 0:
+            return digits
+    return [1] + digits
+
+print(plusOne([1,2,4]))
+    
