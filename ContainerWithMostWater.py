10b67b093a848f0ed26a54aa1c6070c68a87253b
parikhdev <itsyashparikh@gmail.com>
2026-01-13 22:20:01 +0530

    Added ContainerWithMostWater.py

---
+++ ContainerWithMostWater.py
@@ -0,0 +1,19 @@
+def maxArea(height):
+    n = len(height)
+    l = 0
+    r = n - 1
+    max_area = 0
+
+    while l < r:
+        w = r - l
+        h = min(height[l], height[r])
+        a = w * h
+        max_area = max(max_area, a)
+        
+        if height[l] < height[r]:
+            l += 1
+        else:
+            r -= 1
+    return max_area
+
+print(maxArea([1,8,6,2,5,4,8,3,7]))   
