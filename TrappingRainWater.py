649c76bab74ece80c2fe8aa635524d1624b2351c
parikhdev <itsyashparikh@gmail.com>
2026-01-17 23:37:56 +0530

    Added TrappingRainWater.py

---
+++ TrappingRainWater.py
@@ -0,0 +1,22 @@
+def trap(height):
+    l_wall = r_wall = 0
+    n = len(height)
+    max_left = [0] * n
+    max_right = [0] * n
+
+    for i in range(n):
+        j = -i - 1
+        max_left[i] = l_wall
+        max_right[j] = r_wall
+        l_wall = max(l_wall, height[i])
+        r_wall = max(r_wall, height[j])
+
+    summ = 0
+    for i in range(n):
+        pot = min(max_left[i], max_right[i])
+        summ += max(0, pot - height[i])
+
+    return summ
+
+print(trap([4,2,0,3,2,5]))
+    
