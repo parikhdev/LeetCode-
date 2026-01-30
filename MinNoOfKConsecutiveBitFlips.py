fc8759769371b2829107ae8af98551d793a9a813
parikhdev <itsyashparikh@gmail.com>
2026-01-30 23:29:45 +0530

    Added MinNoOfKConsecutiveBitFlips.py

---
+++ MinNoOfKConsecutiveBitFlips.py
@@ -0,0 +1,14 @@
+def minKBitFlips(nums, k):
+    ans = 0
+    flippedTime = 0
+    for i, num in enumerate(nums):
+      if i >= k and nums[i - k] == 2:
+        flippedTime -= 1
+      if flippedTime % 2 == num:
+        if i + k > len(nums):
+          return -1
+        ans += 1
+        flippedTime += 1
+        nums[i] = 2
+    return ans
+print(minKBitFlips([0,1,0],1))
