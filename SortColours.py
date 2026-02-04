b8dcaeb3681f637c752f657aa9c8fede51ca2188
parikhdev <itsyashparikh@gmail.com>
2026-02-05 00:40:20 +0530

    Added SortColours.py

---
+++ SortColours.py
@@ -0,0 +1,16 @@
+def sortColors(nums):
+    left = 0
+    right = len(nums) - 1
+    i = 0
+    while i <= right:
+        if nums[i] == 0:
+            nums[i], nums[left] = nums[left], nums[i]
+            left += 1
+            i += 1
+        elif nums[i] == 2:
+            nums[i], nums[right] = nums[right], nums[i]
+            right -= 1
+        else:
+            i += 1
+    return nums
+print(sortColors([0,1,2,2,1,0,1]))
