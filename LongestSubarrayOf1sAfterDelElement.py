3789dac0bfcdf9be27c7bc061f756f95a177b122
parikhdev <itsyashparikh@gmail.com>
2026-02-06 23:57:37 +0530

    Added LongestSubarrayOf1sAfterDelElement.py

---
+++ LongestSubarrayOf1sAfterDelElement.py
@@ -0,0 +1,18 @@
+def longestSubarray(nums):
+    left = 0
+    zero_count = 0
+    max_length = 0
+
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            zero_count += 1
+
+        while zero_count > 1:
+            if nums[left] == 0:
+                zero_count -= 1
+            left += 1
+
+        max_length = max(max_length, right - left)
+
+    return max_length
+print(longestSubarray([0,1,1,1,1,0]))
