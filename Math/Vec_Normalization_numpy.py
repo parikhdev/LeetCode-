4508c2e99573043f4a063d574154db7fd65014ac
parikhdev <itsyashparikh@gmail.com>
2026-03-08 11:30:48 +0530

    Added Vec_Normalization_numpy.py

---
+++ Math/Vec_Normalization_numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def Normalized_vec(v):
+    vector_array = np.array(v)
+    # linalg -> linear_algebra and norm is the built in function for normalization
+    magnitude = np.linalg.norm(vector_array)
+
+    if magnitude == 0:
+        return v
+    
+    Norm_vec = vector_array/magnitude
+    return Norm_vec
+print(Normalized_vec([3,4]))
